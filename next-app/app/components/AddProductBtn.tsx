717bdd3ce3b3e31de490c9bae192ec581f7737a5
Kaan Uzman <55639112+akaanuzman@users.noreply.github.com>
2024-01-08 14:51:46 +0300

    fetch data from API

---
+++ next-app/app/components/AddProductBtn.tsx
@@ -0,0 +1,10 @@
+'use client';
+import React from 'react'
+
+const AddProductBtn = () => {
+    return (
+        <button onClick={() => console.log("Tapped...")}> Add Product</button>
+    )
+}
+
+export default AddProductBtn
---
+++ next-app/app/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AddProductBtn from './AddProductBtn'
+
+const ProductCard = () => {
+    return (
+        <div>
+            ProductCard
+            <AddProductBtn />
+        </div>
+    )
+}
+
+export default ProductCard
